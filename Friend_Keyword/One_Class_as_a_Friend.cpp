8a0019a77720b950a7600b13df7b240b2fe109cb
aryamanagarwal2003 <aryamanagarwal2105@gmail.com>
2025-03-26 00:00:06 +0530

    Add files via upload

---
+++ Friend_Keyword/One_Class_as_a_Friend.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+class Engine {
+private:
+    int horsepower;
+public:
+    Engine(int hp) : horsepower(hp) {}
+    friend class Car;  // Declare Car as a friend class
+};
+
+class Car {
+public:
+    void showEnginePower(Engine e) {
+        cout << "Engine Power: " << e.horsepower << " HP" << endl;  // ✅ Accessing private data
+    }
+};
+
+int main() {
+    Engine engine(250);
+    Car car;
+    car.showEnginePower(engine);  // ✅ Works due to friend class
+    return 0;
+}
